5242d8e244d518052a1632bb49ca313428149717
PMiskew <paul.miskew@gmail.com>
2019-08-08 10:47:54 -0400

    update

--- Activity 2 - Event Drive Program/GUIVolCylStep1.py
+++ Activity 2 - Event Drive Program/GUIVolCylStep1.py
@@ -1,81 +1,18 @@
-import tkinter as tk
-import math
+
+import math 
 
 
-#VARIABLES
-root = tk.Tk()
-file = open("data.txt","w")
+def calcVolumeCylinder(radius, height):
 
-#FUNCTIONS
+	if radius >= 0 and height >= 0:
+		volume = math.pi*pow(2,2)*height 
+		volume = round(volume,2)
+		return volume	
+	else:
+		return -1
 
-def runMe(*args):
-	print("running")
-	
-	#Access Text
-	r = entRadius.get()
-	h = entHeight.get()
-
-
-	try: 
-		r = float(r)
-		h = float(h)
-
-
-		if (r >= 0 and h >= 0):
-			v = calcVolume(r,h)
-			print(v)
-			display.config(state ="normal")
-			strDisplay = "Volume of Cylinder:\nradius = "+str(r)+"\nheight = "+str(h)+"\nvolume = "+str(v)
-			display.delete(1.0,tk.END)
-			display.insert(tk.END,strDisplay)
-			file.write(str(v)+"\n")
-		else:
-			print("BAD INPUT")
-	except:
-		print("ERROR - STRING INPUT NEED NUMBER")
-
-	entRadius.delete(0,tk.END)
-	entHeight.delete(0,tk.END)
-
-def calcVolume(radius, height):
-
-
-	volume = math.pi*radius*radius*height 
-	volume = round(volume,2)	
-	return volume
-
-
-
-
-
-
-
-
-label = tk.Label(root,text = "Volume Calculator")
-label.pack()
-
-labRadius = tk.Label(root,text = "Radius (cm)")
-labRadius.pack()
-
-entRadius = tk.Entry(root)
-entRadius.pack()
-
-labHeight = tk.Label(root, text = "Height (cm)")
-labHeight.pack()
-entHeight = tk.Entry(root)
-entHeight.pack()
-
-btn = tk.Button(root, text = "Calculate Volume", command = runMe)
-btn.pack();
-
-display = tk.Text(root,height = 5, width = 20)
-display.config(state = "disabled")
-display.pack()
-
-root.bind("<Return>",runMe)
-
-
-#MAIN LOOP
-root.mainloop()
-file.close()
-
+#Main Code:
+print("Start Program")
+result = calcVolumeCylinder(4,3)
+print(result)
+print("End Program")
